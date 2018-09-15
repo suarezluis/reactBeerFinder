292b5930519b97746898db9532a8f428bbd5cc9b
Luis Suarez <suarezluis@gmail.com>
2018-09-15 15:30:07 -0500

    trying to fix cross origin limitation by deploying

--- src/components/Finder.js
+++ src/components/Finder.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import SearchBar from "./SearchBar";
+var http = require("http");
 
 class Finder extends React.Component {
   constructor(props) {
@@ -13,13 +14,13 @@ class Finder extends React.Component {
   }
 
   render() {
-    axios
-      .get(
-        `https://api.brewerydb.com/v2/search/?key=25e5612de5524a1bf68a0933f4b4562a&q=black&type=beer`
-      )
-      .then(res => {
-        console.log(res);
-      });
+    fetch(
+      "https://api.brewerydb.com/v2/search/?key=25e5612de5524a1bf68a0933f4b4562a&q=black&type=beer",
+      { method: "get", mode: "no-cors" }
+    ).then(function(response) {
+      console.log(response.json());
+    });
+
     return (
       <div className="row container">
         {/* <SearchBar onSearchTermChange={term => this.beerSearch(term)} /> */}
