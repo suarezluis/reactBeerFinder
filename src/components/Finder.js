fc59ec6822b69722ef6ffd8611d61e8477a2ba63
Luis Suarez <suarezluis@gmail.com>
2018-09-15 15:03:55 -0500

    trying to fix cross origin limitation by deploying

--- src/components/Finder.js
+++ src/components/Finder.js
@@ -12,17 +12,14 @@ class Finder extends React.Component {
     };
   }
 
-  // beerSearch(term) {
-  //   axios
-  //     .get(
-  //       `https://api.brewerydb.com/v2/search/?key=25e5612de5524a1bf68a0933f4b4562a&q=black&type=beer`
-  //     )
-  //     .then(res => {
-  //       console.log(res);
-  //     });
-  // }
-
   render() {
+    axios
+      .get(
+        `https://api.brewerydb.com/v2/search/?key=25e5612de5524a1bf68a0933f4b4562a&q=black&type=beer`
+      )
+      .then(res => {
+        console.log(res);
+      });
     return (
       <div className="row container">
         {/* <SearchBar onSearchTermChange={term => this.beerSearch(term)} /> */}
